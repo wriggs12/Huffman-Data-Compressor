48c78176ccefdeebd0bec56ed44fc7c7354d32dd
Winston Riggs <64924548+wriggs12@users.noreply.github.com>
2020-09-06 20:21:56 -0400

    Add files via upload

---
+++ BinaryTree.java
@@ -0,0 +1,26 @@
+/**
+ * @aurthor Winston Riggs
+ * @version 9/4/20
+ *
+ * Binary Tree class with a root using the Node class
+ */
+
+public class BinaryTree {
+    private Node root;
+
+    BinaryTree() {
+        root = null;
+    }
+
+    BinaryTree(Node n) {
+        root = n;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node r) {
+        root = r;
+    }
+}
